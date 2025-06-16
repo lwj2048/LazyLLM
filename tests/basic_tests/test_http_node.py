17495a9243c6361e122d1368c4af7412006ed00c
lwj-st <liwenjian.vendor@sensetime.com>
2025-06-16 16:30:01 +0800

    test cache

--- tests/basic_tests/test_http_node.py
+++
@@ -1,9 +0,0 @@
-from lazyllm.tools.http_request.http_request import HttpRequest
-
-class TestHTTPRequest(object):
-
-    def test_http_request(self):
-        http_request = HttpRequest('get', 'https://httpbin.org/ip', api_key='', headers={}, params={}, body='')
-        r = http_request()
-        assert r['status_code'] == 200
-        assert 'origin' in r['content']
